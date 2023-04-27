efabe0ae330c63d330bc22d6ee060c6a1180288c
Christian Alexander Bonnell <33079068+cbonnell28@users.noreply.github.com>
2023-04-27 15:12:45 -0400

    Added ground detection to player
    Player IsGrounded flag now correctly indicates whether the character is on a "Ground" object or not.
    
    Issue: Hanging as far off an edge as possible allows the raycast to not hit the "Ground" and disable our jump even though we are still being supported by the object

--- FPS Raid Practive/Assets/Scripts/PlayerController.cs
+++ FPS Raid Practive/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,13 +11,19 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float Gravity = 20f;
 	[SerializeField] private float JumpForce = 9f;
 
+	public LayerMask GroundCheckLayers = 3;
+
 	private CharacterController controller;
 	private Camera playerCamera;
 	private Vector3 playerVelocity = Vector3.zero;
 	public float lookXLimit = 85.0f;
 	private float rotationX = 0;
-	private bool groundedPlayer = false;
-
+	private bool IsGrounded = false;
+	private Vector3 GroundNormal;
+	private float LastJumpTime;
+	private float GroundCheckDistance = 0.05f;
+	const float JumpGroundingPreventionTime = 0.2f;
+	const float GroundCheckDistanceInAir = 0.07f;
 	private void Start() {
 		controller = GetComponent<CharacterController>();
 		playerCamera = GetComponentInChildren<Camera>();
@@ -28,9 +34,56 @@ public class PlayerController : MonoBehaviour
 	}
 
 	private void Update() {
+		Debug.Log(IsGrounded);
+		GroundCheck();
 		HandlePlayerMovement();
 		HandleJump();
 		HandlePlayerCamera();
+	}
+
+	private void GroundCheck() {
+		// Make sure that the ground check distance while already in air is very small, to prevent suddenly snapping to ground
+		float chosenGroundCheckDistance =
+			IsGrounded ? (controller.skinWidth + GroundCheckDistance) : GroundCheckDistanceInAir;
+
+		IsGrounded = false;
+		GroundNormal = Vector3.up;
+
+		// only try to detect ground if it's been a short amount of time since last jump; otherwise we may snap to the ground instantly after we try jumping
+		if (Time.time >= LastJumpTime + JumpGroundingPreventionTime) {
+			// if we're grounded, collect info about the ground normal with a downward capsule cast representing our character capsule
+			if (Physics.CapsuleCast(GetCapsuleBottomHemisphere(), GetCapsuleTopHemisphere(),
+				controller.radius, Vector3.down, out RaycastHit hit, chosenGroundCheckDistance, -1,
+				QueryTriggerInteraction.Ignore)) {
+				// storing the upward direction for the surface found
+				GroundNormal = hit.normal;
+
+				// Only consider this a valid ground hit if the ground normal goes in the same direction as the character up
+				// and if the slope angle is lower than the character controller's limit
+				if (Vector3.Dot(hit.normal, transform.up) > 0f &&
+					IsNormalUnderSlopeLimit(GroundNormal)) {
+					IsGrounded = true;
+
+					// handle snapping to the ground
+					if (hit.distance > controller.skinWidth) {
+						controller.Move(Vector3.down * hit.distance);
+					}
+				}
+			}
+		}
+	}
+
+	private Vector3 GetCapsuleTopHemisphere() {
+		return transform.position + (transform.up * controller.height/2) - (transform.up * controller.radius);
+	}
+
+	private Vector3 GetCapsuleBottomHemisphere() {
+		return transform.position - (transform.up * controller.height/2) + (transform.up * controller.radius);
+	}
+
+	// Returns true if the slope angle represented by the given normal is under the slope angle limit of the character controller
+	bool IsNormalUnderSlopeLimit(Vector3 normal) {
+		return Vector3.Angle(transform.up, normal) <= controller.slopeLimit;
 	}
 
 	private void HandlePlayerMovement() {
@@ -45,14 +98,15 @@ public class PlayerController : MonoBehaviour
 
 	private void HandleJump() {
 		// Apply jump("Space") if character is on ground
-		if (Input.GetButtonDown("Jump") && groundedPlayer) {
+		if (Input.GetButtonDown("Jump") && IsGrounded) {
 			// Make jump apply only an up force, unmodified by previous vertical velocity
 			playerVelocity = new Vector3(playerVelocity.x, 0f, playerVelocity.z);
 			playerVelocity += Vector3.up * JumpForce;
-			groundedPlayer = false;
+			IsGrounded = false;
+			LastJumpTime = Time.time;
 		}
 
-		if (!groundedPlayer) {
+		if (!IsGrounded) {
 			// Add air accleration to make air movement feel nicer
 
 			playerVelocity += Vector3.down * Gravity * Time.deltaTime;
@@ -66,11 +120,5 @@ public class PlayerController : MonoBehaviour
 		rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
 		playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
 		transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * CameraSensitivity, 0);
-	}
-
-	private void OnControllerColliderHit(ControllerColliderHit hit) {
-		if (hit.gameObject.layer == LayerMask.NameToLayer("Ground")) {
-			groundedPlayer = true;
-		}
 	}
 }
