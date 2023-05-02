f099513412a4f4eb05a119936e25638753cc3d59
Christian Alexander Bonnell <33079068+cbonnell28@users.noreply.github.com>
2023-05-02 18:13:57 -0400

    Fixed Double Tap
    bullets no longer proc damage twice when hitting the player

--- FPS Raid Practive/Assets/Scripts/Bullet.cs
+++ FPS Raid Practive/Assets/Scripts/Bullet.cs
@@ -6,21 +6,34 @@ public class Bullet : MonoBehaviour
 {
     public float damage { get; set; }
     public bool destroyOnCollision = true;
+
+    private bool hasCollided = false;
     private void Start()
     {
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if(!hasCollided)
         {
-            DestroyThisBullet();
-            // Get player object and reduce health by bullet damage
-            PlayerController player = other.GetComponent<PlayerController>();
-            player.HandleDamage(damage);
-        } else if (!other.gameObject.CompareTag("Enemy"))
-        {
-            DestroyThisBullet();
+            if (other.gameObject.CompareTag("Player"))
+            {
+                hasCollided = true;
+                DestroyThisBullet();
+                // Get player object and reduce health by bullet damage
+                PlayerController player = other.GetComponent<PlayerController>();
+                player.HandleDamage(damage);
+            }
+            else if (!other.gameObject.CompareTag("Enemy"))
+            {
+                DestroyThisBullet();
+            }
         }
+    }
+        
+
+    private void OnTriggerExit(Collider other)
+    {
+        hasCollided = false;
     }
 
     private void DestroyThisBullet()
