831282b68835634adac80a6c06c3570d7e223323
xbonnell28 <xbonnell28@gmail.com>
2023-06-05 14:49:06 -0400

    Tutorial and level 2

--- FPS Raid Practive/Assets/LevelExit.cs
+++ FPS Raid Practive/Assets/LevelExit.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelExit : BaseMechanic
 {
     public string targetScene;
+
+    private void Start()
+    {
+        if (isActive)
+        {
+            Activate();
+        }
+    }
+    public override void Activate()
+    {
+        base.Activate();
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        renderer.material.color = Color.yellow;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (isActive)
